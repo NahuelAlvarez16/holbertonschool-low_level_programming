4899db074a0b380c55379b835a9a7e5dc3af3d94
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-06 09:14:40 -0700

    Fix betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *  * main - Print \"Programming is like building a multilingual puzzle
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Print \"Programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
