0ff4e455b3c4f9af1c7a782b9859d15d52801788
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-06 09:09:03 -0700

    Create 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *  * main - Print \"Programming is like building a multilingual puzzle
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
