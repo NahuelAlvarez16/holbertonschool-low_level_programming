e412458e90667aef61997b538b4b5a34f1086fef
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-07 08:05:39 -0700

    Fix

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-	char number;
+	int number;
 
-	for (number = '0'; number  <= '9'; number++)
+	for (number = 0; number  <= 9; number++)
 		putchar(number);
 	putchar('\n');
 	return (0);
