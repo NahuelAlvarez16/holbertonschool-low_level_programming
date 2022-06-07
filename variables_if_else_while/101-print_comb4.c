6067a0b0127c67e3952f8e56c7cbe0691d506203
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-07 14:45:23 -0700

    Create 101-print_comb4

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Print digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hundred;
+	int contador = 0;
+
+	for (hundred = 0; hundred <= 9; hundred++)
+	{
+		int dozen;
+
+		for (dozen = 0; dozen <= 9; dozen++)
+		{
+			int unit;
+
+			for (unit = 0; unit <= 9; unit++)
+			{
+				if (unit > dozen && unit > hundred && dozen > hundred)
+				{
+					if (contador > 0)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					++contador;
+					putchar(hundred % 10 + '0');
+					putchar(dozen % 10 + '0');
+					putchar(unit % 10 + '0');
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
