e55b24aeb1ee3cfa9b83f36bfd674a87f1d7487d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-07 18:46:31 -0700

    Create 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Print with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int position;
+	int len = strlen(messege);
+
+	for (position = 0; position < len; position++)
+	{
+		putchar(msg[position]);
+	}
+	putchar('\n');
+	return (1);
+}
