2f96e84b5e8a7c872bfeef915764b433d586af56
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-09 09:22:10 -0700

    Create 4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Checks for lowercase character.
+ * @c: Value with ASCII code
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	return (0);
+}
