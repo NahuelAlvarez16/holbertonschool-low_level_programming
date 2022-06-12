4aa9cf8abcbe6ecca7e3d4e13779bc2ee2f2ee0b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-11 18:20:13 -0700

    Fix

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Print 0 to 14
+ * @n: Length of line
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		if (i > 9)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
