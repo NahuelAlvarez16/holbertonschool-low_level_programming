4549b15b1a22450b39d52ace46e78edd894559ff
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-11 19:13:39 -0700

    Fix

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - Print 0 to 100
+ * Return: Always 0.
+ */
+void main (void)
+{
+	int i = 0;
+
+	while (i <= 100)
+	{
+		i++
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	_putchar('\n');
+}
