411e60f6f2dba7918dc9a21f9e271ba443a588d3
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-11 18:57:42 -0700

    Create 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - Draws a square
+ * @n: Length of square
+ * Return: Always 0.
+ */
+void print_square(int n)
+{
+	int i = 0;
+
+	for (; i <= n; i++)
+	{
+		int j;
+
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+		}
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
