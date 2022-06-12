164b3991bcc179f937f49f4bad2662273a311bd0
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-12 13:08:20 -0700

    Create 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - Draws a triangle
+ * @size: Length of triangle
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+
+	for (; i <= size; i++)
+	{
+		int j;
+
+		for (j = 0; j < size - i; j++)
+			_putchar(' ');
+		for (j = 0; j < i + 1; i++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
