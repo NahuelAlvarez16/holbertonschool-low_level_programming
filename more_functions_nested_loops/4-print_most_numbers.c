0a49b858c98928d9015aeeba0bf78b01defa5c81
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-11 17:33:22 -0700

    Fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_more_numbers - Print 0 to 9 except 2 and 4
+ * print_most_numbers - Print 0 to 9 except 2 and 4
  * Return: Always 0.
  */
 void print_most_numbers(void)
