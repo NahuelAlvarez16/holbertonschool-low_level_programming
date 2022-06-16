53b1905df73e521a292e9017e7eb0897e55f247d
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-16 11:50:29 -0700

    Create 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Print n elements of an array of integers
+ * @a: Array
+ * @n: Max print for positions
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (*(a + i) && n > i)
+	{
+		printf("%d", *(a + i));
+		i++;
+		if (*(a + i) && i != n)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
