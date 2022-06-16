9f33177f16abefa3fa7c5f719b848adc78b9104f
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-16 12:04:54 -0700

    Create 9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - Copies the string pointed to by src
+ * @dest: Destined
+ * @src: Contens
+ * Return: return src variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
