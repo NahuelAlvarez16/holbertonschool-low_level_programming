a91937e2297330f33e9716befc0bffe9bcc85f0e
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-16 11:30:10 -0700

    Create 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - Print a string
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ int _strlen(char *s);
 void reset_to_98(int *n);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
