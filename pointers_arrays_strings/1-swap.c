2924c30af13506757440fa001b5dd0a0ad31a5d9
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-16 10:50:35 -0700

    Create 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: First number
+ * @b: Second number
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
