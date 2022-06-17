d5f7680e0401da8b2c807ad4aa8deb13cc11a5c7
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-17 07:23:45 -0700

    Fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (dest[length])
 		length++;
 
-	while (src[i] && i < n)
+	while (dest[length] && src[i] && i < n)
 	{
 		*(dest + length) = *(src + i);
 		i++;
