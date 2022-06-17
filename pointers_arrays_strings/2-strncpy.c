615f6bef73bfaf301f90110d00d3b6d5474b5bf3
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-17 07:12:07 -0700

    Create 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - Copies the string pointed to by src
+ * @dest: Destined
+ * @src: Contens
+ * @n: Limit
+ * Return: return src variable
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
