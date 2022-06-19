b4d7bec6cb38331a2af1f91bc48b6f8d6a563b97
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-19 16:31:23 -0700

    Create 5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - Print a string
+ * @str: string
+ * Return: return str in upper
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	_putchar('\n');
+}
