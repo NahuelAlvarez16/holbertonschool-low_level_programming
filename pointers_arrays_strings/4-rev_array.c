ac5b378c6a92e1348c42f657a406a0b15c83417c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-18 19:38:19 -0700

    Fix

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,20 @@
 #include "main.h"
 /**
  * reverse_array - Int in reverse
- * @s: string
+ * @a: Int of array
  * @n: limit
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0;
+	int i = 0, temp;
+
+	n--;
 	while (i < n)
 	{
-		a[i] = a[n - i - 1];
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		n--;
 		i++;
 	}
 }
