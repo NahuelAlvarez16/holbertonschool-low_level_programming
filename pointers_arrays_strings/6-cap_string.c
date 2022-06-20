c36dcc3301830dd7b8a78f8ec11a7f9035fe2e96
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-19 17:22:59 -0700

    Create 6-cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: string
+ * Return: return str
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	char *sep = "\n\t ,;.!?\"(){}"
+
+	while (*(str + i))
+	{
+		int x = 0;
+
+		for (; sep[x] && x >= 0; x++)
+			if (str[i - 1] == sep[x] && str[i] >= 97 && str[i] <= 122)
+			{
+				str[i] = str[i] - 32;
+				x = -1;
+			}
+		i++;
+	}
+	return (str);
+}
