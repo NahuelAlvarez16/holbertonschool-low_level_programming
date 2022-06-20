7f1d546fee4c183b4626d6d97df53f16eed289e2
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-19 18:27:03 -0700

    Create 2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: start string since c
+ * Return: result
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int x = 0;
+	char *result;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			result[x] = s[i];
+			x++;
+		}
+		i++;
+	}
+	return (result);
+}
