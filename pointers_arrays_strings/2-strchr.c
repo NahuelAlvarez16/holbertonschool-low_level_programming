aabf8e36cdb21320fbb4274316386bbbee054177
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-19 18:31:27 -0700

    Fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 	int x = 0;
-	char result[];
+	char *result;
 
 	while (s[i])
 	{
 		if (s[i] == c || x)
 		{
-			result[x] = s[i];
+			*result[x] = s[i];
 			x++;
 		}
 		i++;
