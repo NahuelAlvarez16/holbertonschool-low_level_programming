becc91466275979276b227b7a2257bc491246f82
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-19 17:59:56 -0700

    Fix

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @n: variable
  * Return: return a pointer to the memory area s
  */
-char _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
