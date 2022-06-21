95af444a52a8c13f3434c7a2cee364b68afc6f25
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-21 06:40:59 -0700

    Create 5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _sqrt_recursion_int - the natural square root of a number.
+ * @n: Number
+ * @i: Iterator
+ * Return: the natural square root of a number.
+ */
+int _sqrt_recursion_int(int n, int i)
+{
+	return (n > i ? (i * i == n) ? return (i) :
+		_sqrt_recursion_int(n, i + 1) : return (-1));
+}
+
+/**
+ * _sqrt_recursion - the natural square root of a number.
+ * @n: Number
+ * Return: the natural square root of a number.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursion_int(n, 1));
+}
