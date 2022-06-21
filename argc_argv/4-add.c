eeed2bd0d70c8a39e86201c1da23ed934587a79b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-21 09:32:05 -0700

    Fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
+ * verifyInt - verify if is digit number variable
+ * @number: Number
+ * Return: return 1 if is digit or 0 if isn't digit
+*/
+int verifyInt(char *number)
+{
+	int i = 0;
+
+	for (; number[i]; i++)
+		if (!isdigit(number[i]))
+			return (0);
+	return (1);
+}
+/**
  * main - adds positive numbers.
  * @argc: length of arguments
  * @argv: array of arguments
@@ -15,7 +29,7 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		printf("0\n");
 	for (i = 1; i < argc; i++)
-		if (isdigit(*argv[i]))
+		if (verifyInt(argv[i]))
 		{
 			result += atoi(argv[i]);
 			if (i == argc - 1)
