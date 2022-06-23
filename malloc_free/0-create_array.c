d64a89f43d2535f68f72d71320be092ecdcd2d47
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-23 09:44:53 -0700

    Fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,9 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	if (size > 0)
+		return (NULL);
 	char *temp = malloc(sizeof(char) * (size));
 	unsigned int i = 0;
 
+	if (temp == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		temp[i] = c;
