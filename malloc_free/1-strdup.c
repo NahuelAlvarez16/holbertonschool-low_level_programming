6dc3032ecb0359161de0cc7b8035f7b1cd4b4399
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-23 12:09:41 -0700

    Fix

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup -pointer to a newly allocated space in memory
+ * @str: string
+ * Return: return array
+ */
+char *_strdup(char *str)
+{
+	char *temp;
+	unsigned int i = 0;
+
+	if (str)
+		return (NULL);
+	temp = malloc(sizeof(char) * sizeof(str) + 2);
+
+	if (temp == NULL)
+		return (NULL);
+	while (i < sizeof(str) + 2)
+	{
+		temp[i] = str[i];
+		i++;
+	}
+
+	return (temp);
+}
