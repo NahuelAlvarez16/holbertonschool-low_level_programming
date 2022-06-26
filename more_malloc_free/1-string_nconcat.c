a786621192642cc00900148542ea45a715475925
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-26 15:39:46 -0700

    Fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		temp[i] = s1[i];
 	for (; s2 != NULL && s2[x] && x < n; i++, x++)
 		temp[i] = s2[x];
-	temp[i] = '\0';
+	temp[size1 + size2] = '\0';
 	return (temp);
 }
