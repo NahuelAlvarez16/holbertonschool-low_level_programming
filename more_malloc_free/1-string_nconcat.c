1f1a8e8210fe02bf6faeeb313a9abdf80df26651
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-26 15:31:05 -0700

    Create 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - that concatenates two strings.
+ * @s1: First array.
+ * @s2: Second array.
+ * @n: Limit of second array.
+ * Return: return pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *temp;
+	int size1 = 0;
+	int size2 = 0;
+	int i = 0, x = 0;
+
+	for (; s1 != NULL && s1[size1]; size1++)
+		;
+	for (; s2 != NULL && size2 < n; size2++)
+		;
+	temp = malloc(size1 + size2 + 1);
+
+	if (temp == NULL)
+		exit(NULL);
+	for (; s1 != NULL && s1[i]; i++)
+		temp[i] = s1[i];
+	for (; s1 != NULL && s2[x] && x < n; i++, x++)
+		temp[i] = s2[x];
+	temp[i] = '\0';
+	return (temp);
+}
