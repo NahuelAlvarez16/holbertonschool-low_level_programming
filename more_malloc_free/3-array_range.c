aebe003b178cff0e3520468719b5ce58e9e7e186
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-26 18:06:37 -0700

    Fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	temp = malloc((max - min) * sizeof(int));
+	temp = malloc((max - min + 1) * sizeof(int));
 
 	if (temp == NULL)
 		exit(98);
