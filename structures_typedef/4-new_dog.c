c1896cef0a46f800f8674bf2191e9d60856f3ff6
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-28 11:48:42 -0700

    Fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,11 +10,17 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
 	dog_t *dog_temp = malloc(sizeof(struct dog));
 	if (dog_temp == NULL)
 		return (NULL);
-	strcpy(dog_temp->name, name);
+	for (; name[i]; i++)
+		dog_temp->name[i] = name[i];
+	dog_temp->name[i] = '\0';
+
+	for (i = 0; owner[i]; i++)
+		dog_temp->owner = owner;
+	dog_temp->owner = '\0';
 	dog_temp->age = age;
-	strcpy(dog_temp->owner, owner);
 	return (dog_temp);
 }
