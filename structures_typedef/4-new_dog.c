0c58de30611b043eef15a59d3778d9481e5b0d0c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-28 11:34:49 -0700

    Create 4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - initialize a variable of type struct dog_t
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ * Return: return a dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog_temp = malloc(sizeof(struct dog));
+
+	dog_temp->name = name;
+	dog_temp->age = age;
+	dog_temp->owner = owner;
+	return (dog_temp);
+}
