ff07ea9f344963162313d0f161302a30b9176f09
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-28 12:14:09 -0700

    Fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,14 +10,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog_temp = malloc(sizeof(struct dog));
+	dog_t *dog_temp = malloc(sizeof(dog_t));
 
 	if (dog_temp == NULL)
 		return (NULL);
-	dog_temp->name = malloc(strlen(name));
+
+	dog_temp->name = malloc(strlen(name) + 1);
 	if (dog_temp->name == NULL)
 		return (NULL);
-	dog_temp->owner = malloc(strlen(owner));
+	dog_temp->owner = malloc(strlen(owner) + 1);
 	if (dog_temp->owner == NULL)
 		return (NULL);
 	strcpy(dog_temp->name, name);
