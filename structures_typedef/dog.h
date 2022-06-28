0514e80812e571d629e410848848a31c0c880614
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-28 08:29:16 -0700

    Create dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information of a dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char owner;
+}
+#endif
