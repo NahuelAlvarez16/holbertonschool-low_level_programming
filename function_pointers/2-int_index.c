120b7570db7de8bcc0a9d9c31bed2e22e0492aa9
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-29 08:57:54 -0700

    Fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,11 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (cmp && size && array)
-	{
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
-		return (0);
-	}
 	return (-1);
 }
