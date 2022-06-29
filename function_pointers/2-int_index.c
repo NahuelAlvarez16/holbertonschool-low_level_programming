2566267cddcd3642eb5dfdec2c81ddafd1fa13ce
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-29 08:55:48 -0700

    Create 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer.
+ * @array: Array of ints
+ * @size: Size of Array.
+ * @cmp: Function.
+ * Return: Index of match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp && size && array)
+	{
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+		return (0);
+	}
+	return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void _putchar(char);
+int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
