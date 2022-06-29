400943ccf55368154d5e9c4f4c6cc4b2df182f8f
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-06-29 08:37:24 -0700

    Fix

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,7 +9,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (action && array)
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 }
