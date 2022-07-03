766d5b2fd8b6fd204e32a5bc5a987216900e7832
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-03 15:10:38 -0700

    Fix

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints all parameters with separator
+ * @separator: char separator
+ * @n: length of the parameters
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list arg;
+
+	if (n)
+	{
+		va_start(arg, n);
+		while (i < n - 1)
+		{
+			char *tmp = va_arg(arg, char *);
+
+			printf("%s%s", tmp ? tmp :  "(nil)", separator ? separator : "");
+			i++;
+		}
+		printf("%s", va_arg(arg, char *));
+		va_end(arg);
+	}
+	printf("\n");
+}
