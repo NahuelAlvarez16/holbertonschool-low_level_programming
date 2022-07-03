08ce58fa890c06a8c705a770e5e308eb0c8b3f7a
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-03 14:45:48 -0700

    Create 1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints all parameters with separator
+ * @separator: char separator
+ * @n: length of the parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list arg;
+
+	if (separator != NULL)
+	{
+	va_start(arg, n);
+	while (i < n - 1)
+	{
+		printf("%d%s", va_arg(arg, int), separator);
+		i++;
+	}
+	printf("%d\n", va_arg(arg, int));
+	va_end(arg);
+	}
+}
