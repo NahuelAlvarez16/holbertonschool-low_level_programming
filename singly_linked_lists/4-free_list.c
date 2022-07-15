8e6fa71b3b26c7a4827ec45f7d1e8552f0a6b5cd
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-15 15:20:23 -0700

    Fix free_list function.

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * free_list - free all node of list
  * @head: List
@@ -10,6 +9,8 @@ void free_list(list_t *head)
 	if (head)
 	{
 		free_list(head->next);
+		if (head->str)
+			free(head->str);
 		free(head);
 	}
 }
