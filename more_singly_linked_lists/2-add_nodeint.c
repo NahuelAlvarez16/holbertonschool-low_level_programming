c86a880ef28479048f90b5e1a369405d265611d6
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-15 16:53:26 -0700

    Create add_nodeint function.

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint - Add new node in the list
+ * @head: List
+ * @n: Integer
+ * Return: return address new element in the list
+ */
+list_t *add_nodeint(list_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
