2f7e8a92cdae29edd0073f36f098bcd91472bda5
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-15 17:48:13 -0700

    Create free_listint2 function.

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - free all node of list
+ * @head: List
+ */
+void free_listint2(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
