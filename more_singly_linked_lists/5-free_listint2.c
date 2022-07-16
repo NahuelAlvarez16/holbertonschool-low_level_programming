d8993bb2c952a920084789b09802001acec2dbc3
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-15 17:54:59 -0700

    Fix free_listint2 function.

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -4,11 +4,11 @@
  * free_listint2 - free all node of list
  * @head: List
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	if (head)
+	if (*head)
 	{
-		free_listint(head->next);
-		free(head);
+		free_listint2(&(*head)->next);
+		free(*head);
 	}
 }
