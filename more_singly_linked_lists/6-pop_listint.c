28b7010af3f1c71ec98fb674c12e2970b73a419a
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-15 18:55:00 -0700

    Create pop_listint function

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - Remove first node of list
+ * @head: List
+ * Return: return number of deleted node.
+ */
+int pop_listint(listint_t **head)
+{
+	int number;
+	listint_t *node;
+
+	number = *&(*head)->n;
+	node = *&(*head)->next;
+	free(*head);
+	*head = node;
+	return (number);
+}
