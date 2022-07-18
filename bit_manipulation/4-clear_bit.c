f690737fbd24719af4a7c7cdfd44fe70550d1597
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-18 16:09:17 -0700

    Create clear_bit function

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - That sets the value of a bit to 0 at a given index.
+ * @n: Is a number
+ * @index: is the index, starting form 0
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int power = 1;
+	unsigned int i = 0;
+
+	if (index > 64)
+		return (-1);
+	for (; i < index; i++)
+		power *= 2;
+	if (((*n >> index) & 1))
+		*n -= power;
+	return (1);
+}
