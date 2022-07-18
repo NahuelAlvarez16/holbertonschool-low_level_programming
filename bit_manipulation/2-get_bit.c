4b20bfcb8908dd778bde576b774984a292a4869a
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-18 14:59:48 -0700

    Create get_bit function

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_bit - The value of a bit at a given index
+ * @n: Is a number
+ * @index: is the index, starting form 0
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
