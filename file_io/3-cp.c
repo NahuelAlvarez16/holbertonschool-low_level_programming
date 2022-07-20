673d08e6123352e55735ec1d4f1d40d9342bfd00
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-20 14:38:29 -0700

    Fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,11 +40,6 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	file_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (!file_to)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	code_receiver = read(file_from, content, 1024);
 	while (code_receiver)
 	{
