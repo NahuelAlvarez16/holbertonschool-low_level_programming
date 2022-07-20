cc46fd8129e1d251347033fd84d9c2506bc0591b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-20 15:03:35 -0700

    Fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -41,11 +41,6 @@ int main(int ac, char **av)
 	}
 	file_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	code_receiver = read(file_from, content, 1024);
-	if (!code_receiver)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	while (code_receiver)
 	{
 		if (write(file_to, content, code_receiver) != code_receiver)
@@ -53,7 +48,12 @@ int main(int ac, char **av)
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		code_receiver = read(file_from, content, 1024);	
+		code_receiver = read(file_from, content, 1024);
+		if (code_receiver == -1)
+		{
+			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 	}
 	if (close(file_to) == -1)
 	{
