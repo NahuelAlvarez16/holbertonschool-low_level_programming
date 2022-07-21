069d78f9913fe6af9a09e435bca3645c73e225fa
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-21 08:44:46 -0700

    Creaate add_dnodeint function.

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint - Add new node in the list
+ * @head: List
+ * @n: Integer
+ * Return: return address new element in the list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head->prev = next;
+	*head = new;
+	return (new);
+}
