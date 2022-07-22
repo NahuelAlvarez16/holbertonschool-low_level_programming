35e122c9124aa4f6b8af950b0ef830e079af2e8e
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-22 09:05:24 -0700

    Create sum_dlistint function

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_dlistint - Sum of all the data of the list
+ * @head: List
+ * Return: return sum of all data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int total = 0;
+
+	while (head)
+	{
+		total += head->n;
+		head = head->next;
+	}
+	return (total);
+}
