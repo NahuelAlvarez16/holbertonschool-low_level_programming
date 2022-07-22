9b3c5d741b5e8c86219fe8a2125122df63987d84
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-07-22 08:58:26 -0700

    Fix add_dnodeint_end

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int length = 0;
 
-	while (h->next)
+	while (h)
 	{
 		if (&h->n != NULL)
 			printf("%d\n", h->n);
--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,8 +26,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 	{
-		*head = new;
 		new->prev = NULL;
+		*head = new;
 	}
 	return (new);
 }
