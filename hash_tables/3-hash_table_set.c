91943a82001455e0192895c8a471becd57a54e7b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-19 15:25:23 -0700

    Fix hash_table_set function.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 /**
  * hash_table_set - Function that adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value
  * @key: is the key
  * @value: is the value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
