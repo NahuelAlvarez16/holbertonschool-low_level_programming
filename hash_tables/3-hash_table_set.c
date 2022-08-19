abebe3297ab35db25279fc834d4cb3ab750e39f7
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-19 15:21:02 -0700

    Fix hash_table_set function.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,20 +4,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int index;
 	hash_node_t *new_hash_node;
 
+	if (!ht)
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
 	{
 		return (0);
 	}
-	new_hash_node->key = (char *)key;
-	new_hash_node->value = (char *)value;
+	new_hash_node->key = strdup((char *)key);
+	new_hash_node->value = strdup((char *)value);
 
 	if (!ht->array[index] || strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index] = new_hash_node;
 	}
-       	else
+	else
 	{
 		new_hash_node->next = ht->array[index];
 		ht->array[index] = new_hash_node;
