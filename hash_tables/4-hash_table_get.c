e03f5cbd22763f6fdf636de10e54e4c3c7a7dda7
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-19 15:46:54 -0700

    Fix hash_table_get function

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,10 +8,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index;
-
+	hash_node_t *current_node;
 	if (!ht)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
-
-	return (ht->array[index] ? ht->array[index]->value : NULL);
+	current_node = ht->array[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
 }
