744a654790fcbc9307f27dc9d1372f35f5423fc7
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-18 18:38:17 -0700

    Implement hash_djb2 function.

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if(!hash_table)
+	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
