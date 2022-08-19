91588419ede0813c0dd93d3598d8c4be55b0d627
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-19 15:23:45 -0700

    Fix hash_table_set function.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * hash_table_set - Function that adds an element to the hash table.
+ * @key: is the key
+ * @value: is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ * /
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
