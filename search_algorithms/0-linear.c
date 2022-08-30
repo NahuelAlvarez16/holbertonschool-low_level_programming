9f2cffe49ef4db7d4b4bdde2299fe67b8052f3f1
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-30 15:22:54 -0700

    Fix linear_search function.

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,10 +10,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
-	while (i < size)
+	while (i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (arary[i] === value)
+		if (arary[i] == value)
 			return (i);
 	}
 	return (-1);
